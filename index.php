b8415410c2b9965fc71f2fac236ed3390830146c
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-02-01 08:34:15 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+    session_start();
+?>
+
+<html lang="pt-br">
+    <head>
+         <meta charset="utf-8">
+         <meta name="author" content="Cesar Szpak">
+         <title>Sistema - Login</title>
+    </head>
+    <body>
+          <form method="POST"action="valida.php">
+            <h2>Área Restrita>
+            <label>Email</label>
+            <input type="email" placeholder="Digite seu E-mail" required autofocus>
+            <label>Senha</label>
+            <input type="password" name='senha' placeholder="Digite sua senha" required>
+            <button type="submit">Acessar</button>
+            </h2>
+          </form>
+         <p>
+
+             <?php
+               if (isset($_SESSION['loginErro'])) {
+                   # code...
+                   echo $_SESSION['loginErro'];
+                   unset($_SESSION['loginErro']);
+               }
+            ?>
+
+         </p>
+            
+          
+          <p>
+             <?php
+                 if(isset($_SESSION['logindeslogado'])){
+                     echo $_SESSION['logindeslogado'];
+                     unset($_SESSION['logindeslogado']);
+                 }
+             ?>
+          </p>
+    </body>
+</html>
