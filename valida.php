d04576c2d37ad54b70b948aba65b23e79d1cf4c8
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-02-01 08:56:25 -0300

    criação do arquivo valida.php

---
+++ valida.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+    include_once("conexao.php");
+
+    if ((isset($_POST['email'])) && (isset($_POST['senha']))) {
+        # code...
+        $usuario = mysqli_real_escape_string($conn, $_POST['email']);
+        $senha =   myssqli_real_escape_string($conn, $_POST['senha']);
+        $senha = md5($senha);
+
+        $result_usuario = "SELECT * FROM usuario WHERE email = '$usuario' && senha= '$senha' LIMIT 1";
+        $resultado_usuario = mysqli_query($conn, $result_usuario);
+        $resultado = mysql_fetch_assoc($resultado_usuario);
+
+        if (isset($resultado)) {
+            # code...
+            $_SESSION['usuarioId'] = $resultado['id'];
+            $_SESSION['usuarioNome'] = $resultado['nome'];
+            $_SESSION['usuarioNiveisAcessoId'] = $resultado['niveis_acesso_id'];
+            $_SESSION['usuarioEmail'] = $resultado['email'];
+            if ($_SESSION['usuarioNiveisAcessoId'] == "1") {
+                # code...
+                header('Location: administrativo.php');
+            }elseif($_SESSION['usuarioNiveisAcessoId'] == "2"){
+                header("Location: colaborador.php");
+
+            }else{
+                header("Location: cliente.php");
+            }
+        }else{
+            $_SESSION['loginErro'] = "Usuário ou Senha Inválido";
+            header("Location: index.php");
+        }
+    }else{
+        $_SESSION['loginErro'] = "Usuário ou Senha Inválido";
+        header("Location: index.php");
+    }
+?>
